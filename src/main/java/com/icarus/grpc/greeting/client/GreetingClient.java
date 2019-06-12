d298e962e82e97e81a6da85a04de137c4d870d77
Ic4rus <thanght4391@gmail.com>
2019-06-12 21:33:57 +0700

    Client Streaming API: LongGreet

--- src/main/java/com/icarus/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/icarus/grpc/greeting/client/GreetingClient.java
@@ -4,45 +4,62 @@ import com.proto.dummy.DummyServiceGrpc;
 import com.proto.greet.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class GreetingClient {
 
     public static void main(String[] args) {
         System.out.println("Hello I'm a gRPC client");
 
+        GreetingClient main = new GreetingClient();
+        main.run();
+    }
+
+    private void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
+//        doUnaryCall(channel);
+//        doServerStreamingCall(channel);
 
-        System.out.println("Creating stub");
-        // old dummy
-//        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
-//        DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
+        doClientStreamingCall(channel);
 
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+
+    private void doUnaryCall(ManagedChannel channel) {
         // create a greet service client (blocking - synchronous)
         GreetingServiceGrpc.GreetingServiceBlockingStub greetClient = GreetingServiceGrpc.newBlockingStub(channel);
 
         // Unary
-//        // create a protocol buffer greeting message
-//        Greeting greeting = Greeting.newBuilder()
-//                .setFirstName("Stephane")
-//                .setLastName("Maarek")
-//                .build();
-//
-//        // do same for a GreetRequest
-//        GreetRequest greetRequest = GreetRequest.newBuilder()
-//                .setGreeting(greeting)
-//                .build();
-//
-//        // call the RPC and get back a GreetResponse (protocol buffers)
-//        GreetResponse greetResponse = greetClient.greet(greetRequest);
-//
-//        System.out.println(greetResponse.getResult());
+        // create a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Stephane")
+                .setLastName("Maarek")
+                .build();
+
+        // do same for a GreetRequest
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        // call the RPC and get back a GreetResponse (protocol buffers)
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+        System.out.println(greetResponse.getResult());
+    }
+
+    private void doServerStreamingCall(ManagedChannel channel) {
+        GreetingServiceGrpc.GreetingServiceBlockingStub greetClient = GreetingServiceGrpc.newBlockingStub(channel);
 
         // Server streaming
         // we prepare the request
         GreetManyTimesRequest greetManyTimesRequest = GreetManyTimesRequest.newBuilder()
-                .setGreeting(Greeting.newBuilder().setFirstName("Stephane").build())
+                .setGreeting(Greeting.newBuilder().setFirstName("Stephane"))
                 .build();
 
         // we stream the response (in a blocking manner)
@@ -50,9 +67,68 @@ public class GreetingClient {
                 .forEachRemaining(greetManyTimesResponse -> {
                     System.out.println(greetManyTimesResponse.getResult());
                 });
+    }
 
-        // do something
-        System.out.println("Shutting down channel");
-        channel.shutdown();
+    private void doClientStreamingCall(ManagedChannel channel) {
+        // create an asynchronous client
+        GreetingServiceGrpc.GreetingServiceStub asyncClient = GreetingServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<LongGreetRequest> requestObserver = asyncClient.longGreet(new StreamObserver<LongGreetResponse>() {
+            @Override
+            public void onNext(LongGreetResponse value) {
+                // we get a response from the server
+                System.out.println("Received a response from the server");
+                System.out.println(value.getResult());
+                // onNext will be called only once
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // we get an error from the server
+            }
+
+            @Override
+            public void onCompleted() {
+                // the server is done sending us data
+                // onCompleted will be called right after onNext()
+                System.out.println("Server has completed sending us something");
+                latch.countDown();
+            }
+        });
+
+        // streaming message #1
+        System.out.println("sending message 1");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder()
+                        .setFirstName("Stephane")
+                        .build())
+                .build());
+
+        // streaming message #2
+        System.out.println("sending message 2");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder()
+                        .setFirstName("John")
+                        .build())
+                .build());
+
+        // streaming message #3
+        System.out.println("sending message 3");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder()
+                        .setFirstName("Marc")
+                        .build())
+                .build());
+
+        // we tell the server that client is done sending data
+        requestObserver.onCompleted();
+
+        try {
+            latch.await(3L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
