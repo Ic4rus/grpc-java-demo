4d2e84ce0c6aed73e94a2bf4b3964fe2b338a950
Icarus <thanght4391@gmail.com>
2019-06-13 23:16:44 +0700

    Hands-On SSL Security

--- src/main/java/com/icarus/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/icarus/grpc/greeting/client/GreetingClient.java
@@ -3,30 +3,41 @@ package com.icarus.grpc.greeting.client;
 import com.proto.dummy.DummyServiceGrpc;
 import com.proto.greet.*;
 import io.grpc.*;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import javax.net.ssl.SSLException;
+import java.io.File;
 import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class GreetingClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SSLException {
         System.out.println("Hello I'm a gRPC client");
 
         GreetingClient main = new GreetingClient();
         main.run();
     }
 
-    private void run() {
+    private void run() throws SSLException {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
+
+        // With server authentication SSL/TLS; custom CA root certificates; not on Android
+        ManagedChannel secureChannel = NettyChannelBuilder.forAddress("localhost", 50051)
+                .sslContext(GrpcSslContexts.forClient().trustManager(new File("ssl/ca.crt")).build())
+                .build();
+
 //        doUnaryCall(channel);
 //        doServerStreamingCall(channel);
 //        doClientStreamingCall(channel);
 //        doBiDiStreamingCall(channel);
-        doUnaryCallWithDeadline(channel);
+//        doUnaryCallWithDeadline(channel);
+        doUnaryCall(secureChannel);
 
         System.out.println("Shutting down channel");
         channel.shutdown();
